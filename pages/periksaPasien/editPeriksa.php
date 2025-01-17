443ec86659e561397ac125edc4ad1a81a0194f7a
idrisdzulfikar12 <116345087+idrisdzulfikar12@users.noreply.github.com>
2025-01-17 17:40:15 +0700

    update

--- pages/periksaPasien/editPeriksa.php
+++ pages/periksaPasien/editPeriksa.php
@@ -1,17 +1,37 @@
 <?php
-    require '../../config/koneksi.php';
+require '../../config/koneksi.php';
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $idDaftarPoli = $_POST['id'];
-        $tgl_Periksa = $_POST['tanggal_periksa'];
-        $catatan = $_POST['catatan'];
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $id_daftar_poli = $_POST['id'];
+    $tanggal_periksa = $_POST['tanggal_periksa'];
+    $catatan = $_POST['catatan'];
+    $obat = $_POST['obat']; // Array obat
+    $totalHarga = $_POST['totalHarga']; // Ambil total harga dari form
 
-        $queryUpdate = mysqli_query($mysqli,"UPDATE periksa SET tgl_periksa = '$tgl_Periksa', catatan = '$catatan' WHERE id_daftar_poli = '$idDaftarPoli'");
-        if ($queryUpdate) {
-            echo '<script>alert("Data berhasil diubah");window.location.href="../../periksaPasien.php"</script>';
+    // Update data di tabel periksa
+    $queryPeriksa = "UPDATE periksa 
+                     SET tgl_periksa = '$tanggal_periksa', catatan = '$catatan', biaya_periksa = '$totalHarga' 
+                     WHERE id_daftar_poli = '$id_daftar_poli'";
+    $result = mysqli_query($mysqli, $queryPeriksa);
+
+    if ($result) {
+        // Hapus data lama di detail_periksa
+        $deleteObat = "DELETE FROM detail_periksa WHERE id_periksa = (SELECT id FROM periksa WHERE id_daftar_poli = '$id_daftar_poli')";
+        mysqli_query($mysqli, $deleteObat);
+
+        // Simpan data obat baru ke tabel detail_periksa
+        if (!empty($obat)) {
+            foreach ($obat as $id_obat) {
+                $queryObat = "INSERT INTO detail_periksa (id_periksa, id_obat) 
+                              VALUES ((SELECT id FROM periksa WHERE id_daftar_poli = '$id_daftar_poli'), '$id_obat')";
+                mysqli_query($mysqli, $queryObat);
+            }
         }
-        else{
+
+        if ($result) {
+            echo '<script>alert("Data berhasil diubah");window.location.href="../../periksaPasien.php"</script>';
+        } else {
             echo '<script>alert("Data gagal diubah");window.location.href="../../periksaPasien.php"</script>';
         }
-    }
-?>
+    } 
+}
--- pages/periksaPasien/index.php
+++ pages/periksaPasien/index.php
@@ -35,7 +35,9 @@
                                 <?php 
                                     $no = 1;
                                     require 'config/koneksi.php';
-                                    $query = "SELECT pasien.nama, daftar_poli.keluhan, daftar_poli.status_periksa, daftar_poli.id FROM daftar_poli INNER JOIN pasien ON daftar_poli.id_pasien = pasien.id INNER JOIN jadwal_periksa ON daftar_poli.id_jadwal = jadwal_periksa.id INNER JOIN dokter ON jadwal_periksa.id_dokter = dokter.id WHERE dokter.id = '$id_dokter'";
+                                    $query = "SELECT pasien.nama, daftar_poli.keluhan, daftar_poli.status_periksa, daftar_poli.id FROM daftar_poli INNER JOIN 
+                                    pasien ON daftar_poli.id_pasien = pasien.id INNER JOIN jadwal_periksa ON daftar_poli.id_jadwal = jadwal_periksa.id 
+                                    INNER JOIN dokter ON jadwal_periksa.id_dokter = dokter.id WHERE dokter.id = '$id_dokter'";
                                     $result = mysqli_query($mysqli,$query);
 
                                     while ($data = mysqli_fetch_assoc($result)) {
@@ -50,9 +52,9 @@
                                         ?>
                                         <button type='button' class='btn btn-sm btn-warning edit-btn'
                                             data-toggle="modal"
-                                            data-target="#editModal<?php echo $data['id'] ?>">Edit</button>
-                                            <div class="modal fade" id="editModal<?php echo $data['id'] ?>" tabindex="-1"
-                                            role="dialog" aria-labelledby="addModalLabel" aria-hidden="true">
+                                            data-target="#editModal_<?php echo $data['id'] ?>">Edit</button>
+                                            <div class="modal fade" id="editModal_<?php echo $data['id'] ?>" tabindex="-1"
+                                            role="dialog" aria-labelledby="editModalLabel_<?php echo $data['id']; ?>" aria-hidden="true">
                                             <div class="modal-dialog" role="document">
                                                 <div class="modal-content">
                                                     <div class="modal-header">
@@ -92,6 +94,42 @@
                                                                 <textarea class="form-control" rows="3" id="catatan"
                                                                     name="catatan" required><?php echo $ambilData['catatan'] ?></textarea>
                                                             </div>
+                                                            <!-- Bagian Input Obat pada Modal Edit -->
+                                                            <div class="form-group mb-3">
+                                                                <label>Obat</label>
+                                                                <select class="select2" multiple="multiple" data-placeholder="Pilih Obat" style="width: 100%;" 
+                                                                    name="obat[]" onchange="hitungTotalEdit('editModal_<?php echo $data['id']; ?>')">
+                                                                    <?php 
+                                                                    // Ambil data obat yang sudah dipilih
+                                                                    $selectedObatIds = [];
+                                                                    $ambilObatDipilih = "SELECT id_obat FROM detail_periksa WHERE id_periksa = (SELECT id FROM periksa WHERE id_daftar_poli = '{$data['id']}')";
+                                                                    $resultObatDipilih = mysqli_query($mysqli, $ambilObatDipilih);
+                                                                    while ($row = mysqli_fetch_assoc($resultObatDipilih)) {
+                                                                        $selectedObatIds[] = $row['id_obat'];
+                                                                    }
+
+                                                                    // Tampilkan semua obat
+                                                                    $getObat = "SELECT * FROM obat";
+                                                                    $queryObat = mysqli_query($mysqli, $getObat);
+                                                                    while ($obat = mysqli_fetch_assoc($queryObat)) {
+                                                                        $isSelected = in_array($obat['id'], $selectedObatIds) ? 'selected' : '';
+                                                                    ?>
+                                                                    <option value="<?php echo $obat['id']; ?>" data-harga="<?php echo $obat['harga']; ?>" <?php echo $isSelected; ?>>
+                                                                        <?php echo $obat['nama_obat'] . ' ' . $obat['kemasan'] . ' - Rp.' . number_format($obat['harga'], 0, ',', '.'); ?>
+                                                                    </option>
+                                                                    <?php } ?>
+                                                                </select>
+                                                            </div>
+
+                                                            <!-- Bagian Total Harga pada Modal Edit -->
+                                                            <div class="form-group mb-3">
+                                                                <label>Total Harga</label>
+                                                                <input type="text" class="form-control" id="totalHargaDisplayEdit<?php echo $data['id']; ?>" 
+                                                                    value="Rp.<?php echo number_format($ambilData['biaya_periksa'], 0, ',', '.'); ?>" disabled>
+                                                                <input type="hidden" name="totalHarga" id="totalHargaEdit<?php echo $data['id']; ?>" 
+                                                                    value="<?php echo $ambilData['biaya_periksa']; ?>">
+                                                            </div>
+
                                                             <button type="submit" class="btn btn-success">Simpan</button>
                                                         </form>
                                                     </div>
@@ -137,19 +175,24 @@
                                                             </div>
                                                             <div class="form-group mb-3">
                                                                 <label>Obat</label>
-                                                                <select class="select2" multiple="multiple"
-                                                                    data-placeholder="Pilih Obat" style="width: 100%;"
-                                                                    name="obat[]">
+                                                                <select class="select2" multiple="multiple" data-placeholder="Pilih Obat" style="width: 100%;" name="obat[]" onchange="hitungTotal(this)">
                                                                     <?php 
                                                                         require 'config/koneksi.php';
                                                                         $getObat = "SELECT * FROM obat";
                                                                         $queryObat = mysqli_query($mysqli,$getObat);
                                                                         while ($datas = mysqli_fetch_assoc($queryObat)) {
+                                                                            $infoObat = "{$datas['nama_obat']} - {$datas['qty']} {$datas['kemasan']} - Rp.{$datas['harga']}";
                                                                     ?>
-                                                                    <option value="<?php echo $datas['id'] ?>">
-                                                                        <?php echo $datas['nama_obat'] ?></option>
+                                                                    <option value="<?php echo $datas['id'] ?>" data-harga="<?php echo $datas['harga'] ?>">
+                                                                        <?php echo $infoObat ?>
+                                                                    </option>
                                                                     <?php } ?>
                                                                 </select>
+                                                            </div>
+                                                            <div class="form-group mb-3">
+                                                                <label>Total Harga</label>
+                                                                <input type="text" class="form-control" id="totalHargaDisplay" value="Rp.150000" disabled>
+                                                                <input type="hidden" name="totalHarga" id="totalHarga" value="150000">
                                                             </div>
                                                             <button type="submit" class="btn btn-info">Periksa</button>
                                                         </form>
@@ -174,17 +217,50 @@
 </div>
 <!-- /.content -->
 <script>
-    function hitungTotal() {
-        // Ambil semua elemen select dengan name "obat[]"
-        var selectedObat = document.querySelectorAll('select[name="obat[]"] option:checked');
-        
-        var totalHarga = 150000; // Harga awal
-        // Iterasi melalui obat yang dipilih dan tambahkan harga masing-masing
-        selectedObat.forEach(function(option) {
-            totalHarga += parseInt(option.getAttribute('data-harga')) || 0;
-        });
+    function hitungTotal(selectElement) {
+    // Cari elemen modal terdekat dari elemen select yang berubah
+    var modal = selectElement.closest('.modal');
 
-        // Tampilkan total harga
-        document.getElementById('totalHarga').innerText = 'Total Harga: ' + totalHarga;
+    // Ambil semua option yang dipilih dari select dalam modal tersebut
+    var selectedObat = modal.querySelectorAll('select[name="obat[]"] option:checked');
+
+    var totalHarga = 150000; // Harga awal
+    selectedObat.forEach(function(option) {
+        // Tambahkan harga obat dari atribut data-harga
+        totalHarga += parseInt(option.getAttribute('data-harga')) || 0;
+    });
+
+    // Format harga ke Rupiah dan perbarui tampilan
+    document.getElementById('totalHargaDisplay').value = 'Rp.' + totalHarga.toLocaleString();
+    document.getElementById('totalHarga').value = totalHarga; 
     }
+
+    function hitungTotalEdit(modalId) {
+    // Ambil modal berdasarkan ID
+    var modal = document.getElementById(modalId);
+
+    // Validasi apakah modal ditemukan
+    if (!modal) {
+        console.error('Modal tidak ditemukan:', modalId);
+        return;
+    }
+
+    // Ambil semua option yang dipilih dari select dalam modal tersebut
+    var selectedObat = modal.querySelectorAll('select[name="obat[]"] option:checked');
+
+    var totalHarga = 150000; // Reset total harga
+
+    // Hitung total harga dari obat yang dipilih
+    selectedObat.forEach(function(option) {
+        totalHarga += parseInt(option.getAttribute('data-harga')) || 0;
+    });
+
+    // Format harga ke Rupiah dan perbarui tampilan
+    var totalHargaFormatted = 'Rp.' + totalHarga.toLocaleString();
+
+    // Update elemen input dan tampilan total harga
+    modal.querySelector(`[id^="totalHargaDisplayEdit"]`).value = totalHargaFormatted;
+    modal.querySelector(`[id^="totalHargaEdit"]`).value = totalHarga;
+}
+
 </script>
